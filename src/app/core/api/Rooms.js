64ea498df3c7776b7206ec46d33f750ab7c32502
Sergey Hovakimyan <s.hovakimyan@apollobytes.com>
2017-08-27 19:50:54 +0400

    Fix api calls

--- src/app/core/api/Rooms.js
+++ src/app/core/api/Rooms.js
@@ -1,6 +1,6 @@
 import api from 'packages/api';
 
-export const GetRoomsList = (options?: any): Promise<IServiceResult1<IUserData$1>> => {
-    const uri = '/api/rooms/:id';
+export const GetRoomsList = (Guid: string, options?: any): Promise<IServiceResult1<IUserData$1>> => {
+    const uri = `/api/rooms/${Guid}`;
     return api.get(uri, options).then(response => response.json());
 };
