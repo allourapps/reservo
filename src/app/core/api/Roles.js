f91200a6f8019355fcc017dfb6f8915fb405c038
Eduard <esimonyan2014@gmail.com>
2017-08-27 20:51:01 +0400

    Fix api calls

--- src/app/core/api/Roles.js
+++ src/app/core/api/Roles.js
@@ -1,6 +1,6 @@
 import api from 'packages/api';
 
-export const GetRolesList = (Guid: string, options?: any): Promise<IServiceResult1<IUserData$1>> => {
+export const GetRolesList = (options?: any): Promise<IServiceResult1<IUserData$1>> => {
   const uri = `/api/roles`;
   return api.get(uri, options).then(response => response.json());
 };
